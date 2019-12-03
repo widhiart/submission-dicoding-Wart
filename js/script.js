bf21adb03c61d89619d7a21b847a28f4524fc795
widi Artanto <widhiartanto@gmail.com>
2019-12-03 20:55:53 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,72 @@
+function profile() {
+    document.getElementById('linktransisi').innerHTML = `
+    <div class="transisi">
+    <h1>HI,</h1>
+    <p>My Name, WIDI ARTANTO<br>
+    I am a freelance worker<br>
+    working in the field of marketing<br>
+    and is studying programmers at one of the<br>
+    Bintaro Jaya regional universities </P>
+    </div>
+    `
+}
+
+function about() {
+    document.getElementById('linktransisi').innerHTML = `
+    <div class="transisi">
+    <h1>ABOUT</h1>
+    <table border>
+				<tr>
+					<td>Date of Birth</td>
+					<td>Tangerang/October 18, 1996</td>
+				</tr>
+				<tr>
+					<td>Status</td>
+					<td>Single</td>
+				</tr>
+				<tr>
+					<td>Gender</td>
+					<td>Male</td>
+				</tr>
+				<tr>
+					<td>Height</td>
+					<td>165cm</td>
+				</tr>
+				<tr>
+					<td>Religion</td>
+					<td>Islam</td>
+				</tr>
+				<tr>
+					<td>Home Address</td>
+                    <td>Tm.Cikande Blok C11/22 rt 005/001 Kec. Jayanti Kab.TANGERANG</td>
+				</tr>
+				<tr>
+					<td>Current Address</td>
+					<td>Bintaro - Tangerang Selatan - Banten</td>
+				</tr>
+			</table>
+    </div>
+    `
+}
+
+function contact() {
+    document.getElementById('linktransisi').innerHTML = `
+    <div class="transisi">
+    <h1>CONTACT</h1>
+    <table border>
+				<tr>
+					<td>WhatsApp</td>
+					<td><a href="http://wa.me/62895411910580">0895411910580</a></td>
+				</tr>
+				<tr>
+					<td>Telpon / SMS</td>
+					<td>0895411910580</td>
+				</tr>
+				<tr>
+					<td>E-Mail</td>
+					<td><a href="mailto:widhiartanto@gmail.com">widhiartanto@gmail.com</a></td>
+				</tr>
+			</table>
+    </div>
+    `
+}
